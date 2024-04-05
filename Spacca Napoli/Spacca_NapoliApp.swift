164cdd99b1af061d988d5325ed75a7f8bf07adc2
flashyhuckle <m.glodzik@icloud.com>
2024-04-05 14:18:47 +0200

    first commit

---
+++ Spacca Napoli/Spacca_NapoliApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spacca_NapoliApp.swift
+//  Spacca Napoli
+//
+//  Created by Marcin Głodzik on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Spacca_NapoliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
